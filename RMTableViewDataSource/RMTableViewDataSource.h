8abb6994226fde11f787f5ddef9cba3f236ada87
Robert Mccahill <robert.mccahil@gmail.com>
2019-09-01 16:35:24 +0100

    Initial Commit

---
+++ RMTableViewDataSource/RMTableViewDataSource.h
@@ -0,0 +1,19 @@
+//
+//  RMTableViewDataSource.h
+//  RMTableViewDataSource
+//
+//  Created by Robert Mccahill on 01/09/2019.
+//  Copyright © 2019 Robert Mccahill. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RMTableViewDataSource.
+FOUNDATION_EXPORT double RMTableViewDataSourceVersionNumber;
+
+//! Project version string for RMTableViewDataSource.
+FOUNDATION_EXPORT const unsigned char RMTableViewDataSourceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RMTableViewDataSource/PublicHeader.h>
+
+
